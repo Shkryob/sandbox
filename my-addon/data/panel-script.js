57b0659cccc913d93799f23ca122b5229303e198
through-a-haze <through.a.haze@gmail.com>
2014-10-29 22:58:48 +0200

    clean up

--- my-addon/data/panel-script.js
+++ my-addon/data/panel-script.js
@@ -1,5 +1,4 @@
-    var pic = $('.add-img');
-    console.log('!!!!!');
-    $('button.click').on('click', function(){
-        self.port.emit("from-panel", pic.attr('src'));
-    });
+var pic = $('.add-img');
+$('button.click').on('click', function(){
+    self.port.emit("from-panel", pic.attr('src'));
+});
