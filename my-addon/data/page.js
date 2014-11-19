f8ac6bac5f328cffe37ffb136cecd8d41a223661
Haze <through.a.haze@gmail.com>
2014-11-19 23:53:02 +0200

    upd

--- my-addon/data/page.js
+++ my-addon/data/page.js
@@ -1,33 +1,39 @@
 self.port.on("set-layout", function(pic) {
     $('body').append('<img id="pixel-on-pixel-layout" src="' + pic + '">');
+    layout = $('#pixel-on-pixel-layout');
     
-    $(document).keydown(function(e) {
-    switch (e.which) {
-        case 37:
-            $('#pixel-on-pixel-layout').stop().css({
-                left: '-=1'
-            }); //left arrow key
+    $(document).on('keydown', function(e) {
+        switch (e.which) {
+            case 37:
+                layout.stop().css({
+                    left: '-=1'
+                }); //left arrow key
             break;
-        case 38:
-            $('#pixel-on-pixel-layout').stop().css({
-                top: '-=1'
-            }); //up arrow key
+            case 38:
+                layout.stop().css({
+                    top: '-=1'
+                }); //up arrow key
             break;
-        case 39:
-            $('#pixel-on-pixel-layout').stop().css({
-                left: '+=1'
-            }); //right arrow key
+            case 39:
+                layout.stop().css({
+                    left: '+=1'
+                }); //right arrow key
             break;
-        case 40:
-            $('#pixel-on-pixel-layout').stop().css({
-                top: '+=1'
-            }); //bottom arrow key
+            case 40:
+                layout.stop().css({
+                    top: '+=1'
+                }); //bottom arrow key
             break;
         }
+        offset = layout.offset();
+        xPos = offset.left;
+        yPos = offset.top;
+        self.port.emit("position", xPos, yPos);
+        console.log("position", xPos, yPos);
     });
-    $('#pixel-on-pixel-layout').draggable({
+    layout.draggable({
         drag: function(){
-            offset = $(this).offset();
+            offset = layout.offset();
             xPos = offset.left;
             yPos = offset.top;
             self.port.emit("position", xPos, yPos);
@@ -37,4 +43,5 @@ self.port.on("set-layout", function(pic) {
 
 self.port.on("remove-layout", function() {
     $('#pixel-on-pixel-layout').remove();
+    $(document).off('keydown');
 });
