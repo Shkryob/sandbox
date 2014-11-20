f484e0f7b2bc3d7d718d56eec91782f92b04803f
Haze <through.a.haze@gmail.com>
2014-11-20 23:38:19 +0200

    for test branch

--- my-addon/lib/main.js
+++ my-addon/lib/main.js
@@ -22,23 +22,35 @@ var pageMod = require("sdk/page-mod").PageMod({
 });
 
 var worker;
+var workers = [];
+
+tabs.on("ready", function() {
+    worker_added = false;
+});
+//tabs.on("deactivate", function() {
+//    worker.destroy();
+//    worker_added = false;
+//});
 
 function handleClick() {
     panel.show();
+    console.log(workers);
+    if (worker_added === false) {
+        worker = tabs.activeTab.attach({
+            contentScriptFile: [self.data.url("jquery-2.1.1.min.js"), self.data.url("jquery-ui.min.js"), self.data.url("page.js")]
+        });
+        worker.port.on("position", function(xPos, yPos){
+            panel.port.emit("position", xPos, yPos);
+        });
+        panel.port.on("get-layout", function(pic) {;
+            var abs_url = self.data.url(pic);
+            worker.port.emit("set-layout", abs_url);
+        });
+        panel.port.on("remove-layout", function() {
+            worker.port.emit("remove-layout");
+        });
+        workers.push(worker);
+        worker_added = true;
+    }
 }
 
-worker = tabs.activeTab.attach({
-    contentScriptFile: [self.data.url("jquery-2.1.1.min.js"), self.data.url("jquery-ui.min.js"), self.data.url("page.js")]
-});
-
-worker.port.on("position", function(xPos, yPos){
-    panel.port.emit("position", xPos, yPos);
-});
-
-panel.port.on("get-layout", function(pic) {;
-    var abs_url = self.data.url(pic);
-    worker.port.emit("set-layout", abs_url);
-});
-panel.port.on("remove-layout", function() {
-    worker.port.emit("remove-layout");
-});
