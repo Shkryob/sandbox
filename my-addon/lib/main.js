fb6ea2eebd6b774596137813e40e92cf8c933530
Kostya Shkryob <shkryob@ya.ru>
2014-11-23 23:26:55 +0200

    Handle several tabs

--- my-addon/lib/main.js
+++ my-addon/lib/main.js
@@ -10,10 +10,15 @@ var button = require("sdk/ui/button/action").ActionButton({
     },
     onClick: handleClick
 });
+var pageJS = [self.data.url("jquery-2.1.1.min.js"),
+              self.data.url("jquery-ui.min.js"),
+              self.data.url("page.js")];
 
 var panel = require("sdk/panel").Panel({
     contentURL: self.data.url("panel.html"),
-    contentScriptFile: [self.data.url("jquery-2.1.1.min.js"), self.data.url("panel.js"), self.data.url("img-worker.js")],
+    contentScriptFile: [self.data.url("jquery-2.1.1.min.js"),
+                        self.data.url("panel.js"),
+                        self.data.url("img-worker.js")],
     position: button
 });
 var pageMod = require("sdk/page-mod").PageMod({
@@ -21,24 +26,49 @@ var pageMod = require("sdk/page-mod").PageMod({
     contentStyleFile: self.data.url("page-style.css")
 });
 
-var worker;
-
 function handleClick() {
     panel.show();
+
+    if (!tabs.activeTab.worker) {
+        
+        attachJSToPage(tabs.activeTab);
+        
+        tabs.activeTab.on('ready', function(tab) {
+            attachJSToPage(tab);
+            if (tab.layoutActive) {
+                setLayout(tab, tab.picURL);
+            }
+        });
+    }
 }
 
-worker = tabs.activeTab.attach({
-    contentScriptFile: [self.data.url("jquery-2.1.1.min.js"), self.data.url("jquery-ui.min.js"), self.data.url("page.js")]
+function attachJSToPage(tab) {
+    var worker = tab.attach({
+        contentScriptFile: pageJS
+    });
+
+    worker.port.on("position", function(xPos, yPos){
+        panel.port.emit("position", xPos, yPos);
+    });
+    
+    tab.worker = worker;
+}
+
+function setLayout(tab, pic) {
+    var absURL = self.data.url(pic);
+    var worker = tab.worker;
+    worker.port.emit("set-layout", absURL);
+    tab.layoutActive = true;
+    tab.absPicURL = absURL;
+    tab.picURL = pic;
+}
+
+panel.port.on("get-layout", function(pic) {
+    setLayout(tabs.activeTab, pic)
 });
 
-worker.port.on("position", function(xPos, yPos){
-    panel.port.emit("position", xPos, yPos);
-});
-
-panel.port.on("get-layout", function(pic) {;
-    var abs_url = self.data.url(pic);
-    worker.port.emit("set-layout", abs_url);
-});
 panel.port.on("remove-layout", function() {
+    var worker = tabs.activeTab.worker;
     worker.port.emit("remove-layout");
+    tabs.activeTab.layoutActive = false;
 });
